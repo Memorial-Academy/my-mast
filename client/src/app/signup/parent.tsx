5f9f3dab8b2ed97c7dabfaec4b872a5bcd362b79
Griffin Ferguson <griffinferguson.us@gmail.com>
2024-07-16 04:05:36 -0500

    client: more efficient component separation for signup form

--- client/src/app/signup/parent.tsx
+++ client/src/app/signup/parent.tsx
@@ -3,23 +3,6 @@ import LabelledInput from "@/components/LabelledInputs";
 export default function ParentSignupPage() {
     return (
         <>
-            {/* <input style={{display: "none"}} value="parent" name="role" tabIndex={-1}/> */}
-
-            <h2>Let's start with some basic info...</h2>
-            <LabelledInput
-                question="Email address"
-                placeholder="Email"
-                required
-                name="email"
-            />
-            <LabelledInput
-                question="Password"
-                placeholder="Password"
-                required
-                protected
-                name="password"
-            />
-
             <h2>Parent Information</h2>
             <LabelledInput
                 question="First Name"
--- client/src/components/auth/SignupForm.tsx
+++ client/src/components/auth/SignupForm.tsx
@@ -3,6 +3,7 @@ import LoginButton from "@/components/auth/LoginButton";
 import { useState } from "react";
 import { MultipleChoice } from "../LabelledInputs";
 import { signupUser } from "@/app/lib/auth";
+import LabelledInput from "../LabelledInputs";
 
 import ParentSignupPage from "@/app/signup/parent";
 
@@ -26,7 +27,25 @@ export default function SignupForm() {
                 />
             </form>
             <form id="signup" action={signupUser}>
+                {(userRole != "") && <>
+                    <h2>Let's start with some basic info...</h2>
+                    <LabelledInput
+                        question="Email address"
+                        placeholder="Email"
+                        required
+                        name="email"
+                    />
+                    <LabelledInput
+                        question="Password"
+                        placeholder="Password"
+                        required
+                        protected
+                        name="password"
+                    />
+                </>}
+
                 {(userRole == "parent") && <ParentSignupPage />}
+                
                 {(userRole != "") && <>
                     <input type="checkbox" name="agreement" id="agreement" required />
                     <label htmlFor="agreement">I agree to the Privacy Policy and Terms of Service for the Memorial Academy of Science and Technology</label>
