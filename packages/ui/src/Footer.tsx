a34a71776f816f1ba33cad587d46b4e2ccb8ef6a
Griffin Ferguson <griffinferguson.us@gmail.com>
2025-07-08 03:09:12 -0500

    ui: add: global footer

---
+++ packages/ui/src/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/footer.css";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Memorial Academy of Science and Technology &copy; 2025</p>
+            <p>MyMAST is a custom software solution developed by the Memorial Academy of Science and Technology. <a href="https://memorialacademy.org/about/mymast" target="_blank">Click here to learn more.</a>.</p>
+            <p>
+                <a href="https://memorialacademy.org">Main Website</a>
+                &nbsp;|&nbsp;
+                <a href="https://memorialacademy.org/donate">Donate</a>
+                &nbsp;|&nbsp;
+                <a href="https://memorialacademy.org/privacy">Privacy</a>
+            </p>
+        </footer>
+    )
+}
--- packages/ui/src/MainLayout.tsx
+++ packages/ui/src/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import "../styles/globals.css"
 import { Loading } from "./Loading";
+import Footer from "./Footer";
 
 export function Layout({children}: {children: React.ReactNode}) {
     return (
@@ -8,6 +9,7 @@ export function Layout({children}: {children: React.ReactNode}) {
             {/* <Suspense fallback={<Loading/>}> */}
                 {children}
             {/* </Suspense> */}
+            <Footer/>
         </body>
     )
 }
